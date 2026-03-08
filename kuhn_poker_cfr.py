297a996c07e9ab0bf50ffe7ce4030737765d7474
godmode1111 <71916438+rigba@users.noreply.github.com>
2026-03-08 21:12:00 +1030

    Interactive terminal flow

--- kuhn_poker_cfr.py
+++ kuhn_poker_cfr.py
@@ -112,7 +112,174 @@ def cfr(cards: list[int], history: str, p0: float, p1: float) -> float:
     return node_util
 
 
-train(1000000)
+print("Welcome to a Kuhn Poker CFR Implementation")
+
+iterations = -1
+
+while True:
+    try:
+        iterations = int(input("How many iterations would you like to train on?: "))
+        if iterations < 0:
+            print("Please enter a non-negative integer.")
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid integer.")
+
+train(iterations)
+
+response = ""
+
+def card_no_as_royal(no):
+    match no:
+        case 1:
+            return "Jack"
+        case 2:
+            return "Queen"
+        case 3:
+            return "King"
+        case _:
+            return "Invalid card"
+        
+def action_to_pass_bet(no, natural_language: bool):
+    match no:
+        case 0:
+            return  "Pass" if natural_language else "p"
+        case 1:
+            return "Bet" if natural_language else "b"
+        case _:
+            return "Invalid Action"
+        
+def action_acronym_to_action_no(str):
+    if str == "p":
+        return 0
+    if str == "b":
+        return 1
+    return -1
+        
+def pass_or_bet_input():
+    while True:
+        try:
+            response = input("Would you like to pass or bet? P/B: ")
+            if response != "P" and response != "B":
+                print("Please enter Pass(P) or Bet(B)")
+
+            return response
+        except ValueError:
+            print("Please enter Pass(P) or Bet(B)")
+            
+def get_bot_action(strategy: list[float]): # map mixed strategy dist to action
+    r = random.random()
+    a: int = 0
+    cumulative_probability: float = 0
+    
+    while(a < NUM_ACTIONS - 1):
+        cumulative_probability += strategy[a]
+        if (r < cumulative_probability):
+            break
+        a += 1
+    
+    return a
+
+
+def is_terminal(history: str):
+    return history in ["pp", "bp", "pbp", "bb"]
+
+def get_payout(history:str, cards: list[int]) -> int:
+    
+    is_player_card_higher = cards[0] > cards[1]
+  
+    if history == "pp":
+        return 1 if is_player_card_higher else -1
+    elif history == "bp":
+        return 1      # player bet, bot folded
+    elif history == "pbp":
+        return -1     # bot bet, player folded
+    elif history == "bb":
+        return 2 if is_player_card_higher else -2  
+
+while True:
+    try:
+        response = input("Would you like to play against the learnt strategy? Y/N: ")
+        if response != "Y" and response != "N":
+            print("Please enter Yes(Y) or No(N)")
+            continue
+        break
+    except ValueError:
+        print("Please enter Yes(Y) or No(N)")
+        
+if response == "N":
+    print("Very well, thankyou for checking out my project!")
+else:
+    play_again = True
+    player_tally = 0
+    bot_tally = 0
+    who_is_first = 0 # 0 human, 1 bot
+    while play_again:
+        cards = [1, 2, 3]
+        random.shuffle(cards)
+        player_card = cards[0]
+        bot_card = cards[1]
+        history = ""
+        current_player = 0 if who_is_first == 0 else 1
+        
+        if who_is_first == 0:
+            print("You are first! You drew a " + card_no_as_royal(player_card))
+        else:
+            print("You are second! You drew a " + card_no_as_royal(player_card))
+            
+        
+        while not is_terminal(history):
+            if current_player == 0:
+                action = action_acronym_to_action_no(pass_or_bet_input().lower())
+            else:
+                action = get_bot_action(node_map[str(bot_card) + history].strategy)
+                print("Your opponent chose to " + action_to_pass_bet(action, True))
+
+            history += action_to_pass_bet(action, False)
+            current_player = 1 if current_player == 0 else 0
+        
+        
+        if history[-2:] == "bb":
+            print("Showdown time...*queue drum roll*")
+            print("You drew a " + card_no_as_royal(player_card) + " and your opponent drew a " + card_no_as_royal(bot_card))
+            
+        if history[-1:] == "p":
+            last_player = 1 if current_player == 0 else 0
+
+            if last_player == 0:
+                print("You played a terminal pass action.")
+            else:
+                print("The bot played a terminal pass action.")
+                
+        payout = get_payout(history, cards[:2])
+        
+        if payout < 0:
+            print("You lost this hand!")
+        else:
+            print("You won this hand!")
+            
+        player_tally += payout
+        bot_tally += -payout
+        
+        print(f"Total Scores: You: {player_tally}, Bot: {bot_tally}")
+        
+        play_again_input = ""
+        
+        while True:
+            try:
+                play_again_input = input("Would you like to play again? Y/N: ")
+                if play_again_input != "Y" and play_again_input != "N":
+                    print("Please enter Yes(Y) or No(N)")
+                    continue
+                break
+            except ValueError:
+                print("Please enter Yes(Y) or No(N)")
+                
+        if play_again_input == "Y":
+            continue
+        else:
+            break
     
 
             
